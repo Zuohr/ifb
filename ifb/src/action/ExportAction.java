d43401af6f79758ce3b75660e25201063fa040d1
Zuohr <haoran.zuo@gmail.com>
2014-06-05 15:05:22 -0400

    add jars

--- ifb/src/action/ExportAction.java
+++ ifb/src/action/ExportAction.java
@@ -3,11 +3,7 @@ package action;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -18,10 +14,6 @@ import model.Model;
 public class ExportAction implements Action {
 	private String downloadPath;
 	
-	private boolean compressed = false;
-	
-	private boolean downloaded = false;
-
 	public ExportAction(Model model) {
 		downloadPath = model.getDownloadPath();
 	}
@@ -29,31 +21,10 @@ public class ExportAction implements Action {
 	@Override
 	public String perform(HttpServletRequest request,
 			HttpServletResponse response) throws IOException {
-		String dirName = downloadPath + request.getSession().getId();
-		File dir = new File(dirName);
-		File[] files = dir.listFiles();
+		File dir = new File(downloadPath + request.getSession().getId());
 		String zipFileName = "privacy_form.zip";
-		ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(
-				new File(dir, zipFileName)));
-		ZipEntry ze = null;
-		int buffer_size = 4096;
-		byte[] buf = new byte[buffer_size];
-		for (int i = 0; i < files.length; i++) {
-			File file = (File) files[i];
-			ze = new ZipEntry(getAbsFileName(dirName, file));
-			ze.setSize(file.length());
-			ze.setTime(file.lastModified());
-			zos.putNextEntry(ze);
-			InputStream is = new BufferedInputStream(new FileInputStream(file));
-			int readLen = -1;
-			while ((readLen = is.read(buf, 0, buffer_size)) != -1) {
-				zos.write(buf, 0, readLen);
-			}
-			is.close();
-		}
-		zos.close();
 
-		File downLoadfile = new File(dirName, zipFileName);
+		File downLoadfile = new File(dir, zipFileName);
 		response.setContentType("application/octet-stream");
 		response.setContentLength(new Long(downLoadfile.length()).intValue());
 		response.addHeader("Content-Disposition", "attachment; filename="
@@ -62,6 +33,7 @@ public class ExportAction implements Action {
 		ServletOutputStream sos = response.getOutputStream();
 		BufferedInputStream bis = new BufferedInputStream(new FileInputStream(
 				downLoadfile));
+		int buffer_size = 4096;
 		int readLen = -1;
 		byte[] b = new byte[buffer_size];
 		while ((readLen = bis.read(b)) != -1) {
@@ -71,33 +43,12 @@ public class ExportAction implements Action {
 		sos.close();
 		bis.close();
 
-		for (File file : dir.listFiles()) {
-			file.delete();
-		}
-		dir.delete();
-
 		return "";
 	}
 
 	@Override
 	public String getName() {
 		return "export.do";
-	}
-
-	private String getAbsFileName(String baseDir, File realFileName) {
-		File real = realFileName;
-		File base = new File(baseDir);
-		String ret = real.getName();
-		while (true) {
-			real = real.getParentFile();
-			if (real == null)
-				break;
-			if (real.equals(base))
-				break;
-			else
-				ret = real.getName() + "/" + ret;
-		}
-		return ret;
 	}
 
 }
--- ifb/src/action/GenerateAction.java
+++ ifb/src/action/GenerateAction.java
@@ -1,22 +1,27 @@
 package action;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import json.JsonData;
+import model.Model;
 
 import com.google.gson.Gson;
 
-import model.Model;
-
 public class GenerateAction implements Action {
-	
+
 	private String downloadPath;
-	
+
 	public GenerateAction(Model model) {
 		this.downloadPath = model.getDownloadPath();
 	}
@@ -25,17 +30,70 @@ public class GenerateAction implements Action {
 	public String perform(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		File targetDir = new File(downloadPath + request.getSession().getId());
+		if (targetDir.exists()) {
+			for (File file : targetDir.listFiles()) {
+				file.delete();
+			}
+			targetDir.delete();
+			targetDir = new File(downloadPath + request.getSession().getId());
+		}
 		targetDir.mkdir();
-		PrintWriter pw = new PrintWriter(new FileWriter(new File(targetDir, "test.html")));
+		PrintWriter pw = new PrintWriter(new FileWriter(new File(targetDir,
+				"test.html")));
 		pw.println("hello");
 		pw.close();
-		
+
 		pw = new PrintWriter(new FileWriter(new File(targetDir, "test.json")));
 		Gson json = new Gson();
 		pw.print(json.toJson(new JsonData()));
 		pw.close();
+
+		compress(request);
+
 		return "";
-		
+
+	}
+
+	private void compress(HttpServletRequest request) throws Exception {
+		String dirName = downloadPath + request.getSession().getId();
+		File dir = new File(dirName);
+		File[] files = dir.listFiles();
+		String zipFileName = "privacy_form.zip";
+		ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(
+				new File(dir, zipFileName)));
+		ZipEntry ze = null;
+		int buffer_size = 4096;
+		byte[] buf = new byte[buffer_size];
+		for (int i = 0; i < files.length; i++) {
+			File file = (File) files[i];
+			ze = new ZipEntry(getAbsFileName(dirName, file));
+			ze.setSize(file.length());
+			ze.setTime(file.lastModified());
+			zos.putNextEntry(ze);
+			InputStream is = new BufferedInputStream(new FileInputStream(file));
+			int readLen = -1;
+			while ((readLen = is.read(buf, 0, buffer_size)) != -1) {
+				zos.write(buf, 0, readLen);
+			}
+			is.close();
+		}
+		zos.close();
+	}
+
+	private String getAbsFileName(String baseDir, File realFileName) {
+		File real = realFileName;
+		File base = new File(baseDir);
+		String ret = real.getName();
+		while (true) {
+			real = real.getParentFile();
+			if (real == null)
+				break;
+			if (real.equals(base))
+				break;
+			else
+				ret = real.getName() + "/" + ret;
+		}
+		return ret;
 	}
 
 	@Override
